37f9cd9b43f83190c0929b9bc3c0cd96c25b2f7f
Nx-One <69207158+Nx-One@users.noreply.github.com>
2022-11-08 23:41:45 +0700

    Add files via upload

---
+++ cust myharuisyou.py
@@ -0,0 +1,14 @@
+Nilai1 = 3
+Nilai2 = 5
+
+if Nilai1%2 == 0:
+    print("Nilai 1 = Genap")
+else:
+    print("Nilai 1 = Ganjil")
+
+Nilai3 = Nilai1 + Nilai2
+
+if Nilai3 >= 100:
+    print("Angka Besar")
+else:
+    print("Angka Kecil")
